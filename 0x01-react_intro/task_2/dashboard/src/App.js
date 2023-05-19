6ff628df5e5198781a8cf080c826ab02fd4a37fc
Nsisong-Akpakpan <93917408+Nsisong-Akpakpan@users.noreply.github.com>
2023-05-19 15:56:23 +0100

    Modify the App && Modify the Notifications

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+const year = getFullYear();
+const getFooter = getFooterCopy(false);
+
+export default function App() {
+    return (
+        <div>
+            <header className="App-header">
+                <img src={logo} alt="logo"></img>
+                <h1>School dashboard</h1>
+            </header>
+            <div className="divider"></div>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+                <label htmlFor="Email">Email: </label>
+                <input type="email" id="Email" name="Email"/>
+                <label id="appLabel2"  htmlFor="Password"> Password: </label>
+                <input type="password" id="Password" name="Password"/>
+                <button >Ok</button>
+         
+            </div>
+            <div className="divider"></div>
+            <footer className="App-footer">
+                <p>Copyright {year} - {getFooter}</p>
+            </footer>
+        </div>
+    );
+}
+  
