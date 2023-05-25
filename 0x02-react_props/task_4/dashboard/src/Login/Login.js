97f63309698007caee0ff4711d88021c91ad4d0e
Nsisong-Akpakpan <93917408+Nsisong-Akpakpan@users.noreply.github.com>
2023-05-25 17:01:23 +0100

    all files

--- 0x02-react_props/task_4/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_4/dashboard/src/Login/Login.js
@@ -8,15 +8,17 @@ const getFooter = getFooterCopy(false);
 export default function Login() {
     return (
         <div>
-            <div className="App-body">
-                <p>Login to access the full dashboard</p>
-                <label htmlFor="Email">Email: </label>
-                <input type="email" id="Email" name="Email"/>
-                <label htmlFor="Password"> Password: </label>
-                <input type="password" id="Password" name="Password"/>
-                <button>Ok</button>
-            </div>
-            <div className="linea"></div>
+        <div className="App-body">
+         <p>Login to access the full dashboard</p>
+         <form>
+             <label htmlFor="email">Email:</label>
+             <input type="email" name="email"></input>
+             <label id="appLabel2" htmlFor="password">Password:</label>
+             <input type="password" name="password"></input>
+             <button>OK</button>
+         </form>
+        </div>
+         <div className="divider"></div>
         </div>
     );
 }
